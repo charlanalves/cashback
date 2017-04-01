d942c150a83369933983ced98e3c779bedda0218
charlanalves <charlan.job@gmail.com>
2017-04-01 11:20:51 -0300

    prod enviroment

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=cashback',
             'username' => 'root',
-            'password' => '',
+            'password' => '@by02016Abio#',
             'charset' => 'utf8',
         ],
         'mailer' => [
