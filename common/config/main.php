0438abd8c6bdc72d4c1a2d053dd2e7ed35a38989
charlanalves <charlan.job@gmail.com>
2017-04-02 16:28:33 -0300

    homeurl

--- common/config/main.php
+++ common/config/main.php
@@ -19,7 +19,8 @@ return [
     'as access' => [
         'class' => 'mdm\admin\components\AccessControl',
         'allowActions' => [
-            'admin/*',          
+            'site/login',          
+            'site/logout',          
             
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,10 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+     
+        
     ],
+    'homeUrl' => 'empresa',
+    'defaultRoute' => 'empresa',
     'params' => $params,
 ];
