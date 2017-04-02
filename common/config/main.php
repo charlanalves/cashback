a473c6101f634f1b7e421cc890d95669d8150a7e
charlanalves <charlan.job@gmail.com>
2017-04-02 18:00:47 -0300

    allowed permisson

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
@@ -23,6 +21,7 @@ return [
         'allowActions' => [
             'site/login',          
             'site/logout',          
+            'admin/*'
             
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
