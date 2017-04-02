f11c6a934a10061a2dce50018f3b71df78722323
charlanalves <charlan.job@gmail.com>
2017-04-01 21:02:35 -0300

    Role and Permissions

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -16,13 +16,24 @@ return [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
-        'authManager' =>[
-            'class' => 'yii\rbac\DbManager',
-        ],
-        'modules' => [
-            'admin' => [
-                'class' => 'mdm\admin\Module'
-            ]
+     
+    ],
+     'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
         ]
     ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'admin/*',          
+            
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],
+
 ];
