9ee05e0176ad2421501135a84c03a05773370a3c
eduardomatias <eduardomatias.1989@gmail.com>
2017-05-10 00:44:00 -0300

    novo login

--- common/config/main.php
+++ common/config/main.php
@@ -54,7 +54,7 @@ return [
             'administrador/logout',
 
 
-            'api-empresa/promocao',
+            'api-empresa/*',
             
             // The actions listed here will be allowed to everyone including guests.
             // So, 'admin/*' should not appear here in the production, of course.
--- frontend/controllers/ApiEmpresaController.php
+++ frontend/controllers/ApiEmpresaController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use common\controllers\GlobalBaseController;
+use common\models\LoginForm;
 use common\models\CB06VARIACAO;
 
 /**
@@ -10,22 +11,53 @@ use common\models\CB06VARIACAO;
  */
 class ApiEmpresaController extends GlobalBaseController {
 
-    private $user;
     public $url;
-
+    
     public function __construct($id, $module, $config = []) {
-        $this->user = \Yii::$app->user->identity;
         $this->url = \Yii::$app->request->hostInfo . '/cashback/frontend/web/';
         parent::__construct($id, $module, $config);
     }
+    
     
     public function beforeAction($action) {
         $this->enableCsrfValidation = false;
         return parent::beforeAction($action);
     }
 
+    /**
+     * Index.
+     */
+    public function actionIndex() {
+        //return $this->redirect(\yii\helpers\Url::to('index.php?r=api-empresa/login'));
+    }
+
+    
+    /**
+     * Login
+     */
+    public function actionLogin() {
+        $model = new LoginForm();
+        $model->setAttributes(\Yii::$app->request->post());
+        $model->loginCpfCnpj(); 
+        return json_encode(($model->errors ? ['error' => $model->errors] : \Yii::$app->user->identity->attributes));
+    }
+
+    
+    /**
+     * Login Active
+     */
+    public function actionLoginActive() {
+        $model = \common\models\User::findOne(['auth_key' => \Yii::$app->request->post('auth_key')]);
+        return json_encode(($model ? $model->attributes : ['error' => [[['Faça o login']]]]));
+    }
+    
+    
+    /**
+     * Promocoes
+     */
     public function actionPromocao() {
         $promocao = CB06VARIACAO::getPromocao($this->url);
         return json_encode($promocao);
     }
+
 }
