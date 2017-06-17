dd524affa552ac54a888c6580e928ce665ae0414
eduardomatias <eduardomatias.1989@gmail.com>
2017-06-16 23:38:01 -0300

    saldo - view saldo cliete

--- common/models/VIEWEXTRATOCLIENTE.php
+++ common/models/VIEWEXTRATOCLIENTE.php
@@ -27,15 +27,35 @@ class VIEWEXTRATOCLIENTE extends BaseVIEWEXTRATOCLIENTE
         ]);
     }
     
-  public static function saldoAtualByCliente($cliente)
+    public static function saldoAtualByCliente($cliente) 
     {
-        $sql = "SELECT SUM(VLR2) AS SALDO FROM VIEW_EXTRATO_CLIENTE WHERE CLIENTE = :cliente";
+        $sql = "SELECT SUM(VLR2) AS SALDO FROM VIEW_EXTRATO_CLIENTE WHERE CLIENTE = :cliente AND DT2 IS NOT NULL";
         $command = \Yii::$app->db->createCommand($sql);
         $command->bindValue(':cliente', $cliente);
         return $command->queryOne()['SALDO'];
     }
     
+    public static function saldoPendenteByCliente($cliente) 
+    {
+        $sql = "SELECT SUM(VLR2) AS SALDO FROM VIEW_EXTRATO_CLIENTE WHERE CLIENTE = :cliente AND DT2 IS NULL";
+        $command = \Yii::$app->db->createCommand($sql);
+        $command->bindValue(':cliente', $cliente);
+        return $command->queryOne()['SALDO'];
+    }
     
+    public static function saldoAtualePendenteByCliente($cliente) 
+    {
+        $sql = "SELECT 
+                SUM(CASE WHEN DT2 IS NOT NULL THEN VLR2 ELSE 0 END) AS SALDO_LIBERADO,
+                SUM(CASE WHEN DT2 IS NULL THEN VLR2 ELSE 0 END) AS SALDO_PENDENTE
+                FROM VIEW_EXTRATO_CLIENTE 
+                WHERE CLIENTE = :cliente
+                GROUP BY CLIENTE";
+        $command = \Yii::$app->db->createCommand($sql);
+        $command->bindValue(':cliente', $cliente);
+        return $command->queryOne();
+    }
+
     public static function saldoAtualByAuthKey($AuthKey)
     {
         return self::saldoAtualByCliente(User::getIdByAuthKey($AuthKey));
