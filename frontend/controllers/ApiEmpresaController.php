88a9226e02334a30f81a14f90af360efe1e664f8
Charlan Santos <charlan.job@gmail.com>
2017-07-20 23:13:54 -0700

    email

--- frontend/controllers/ApiEmpresaController.php
+++ frontend/controllers/ApiEmpresaController.php
@@ -172,7 +172,7 @@ class ApiEmpresaController extends GlobalBaseController {
         $link = $this->urlController . 'valid-mail&c='. $post['auth_key'];
         $texto = SYS01PARAMETROSGLOBAIS::getValor('TX_MAIL') . "<br />" . $link;
         
-        \Yii::$app->mail->compose('teste')
+        \Yii::$app->mail->compose('confirmacaoemail')
         ->setFrom('nao-responda@estalecas.com.br')
         ->setTo($post['email'])
         ->setSubject('E$TALECA - Confirmação de E-mail')
