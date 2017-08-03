8755c77354cae23e2f4abc306c47dd4556fa84e4
Charlan Santos <charlan.job@gmail.com>
2017-08-02 23:00:45 -0700

    correção reenviar email

--- frontend/mail/confirmacaoemail.php
+++ frontend/mail/confirmacaoemail.php
@@ -4,16 +4,10 @@ use yii\helpers\Url;
 
 /* @var $this \yii\web\View view component instance */
 /* @var $message \yii\mail\BaseMessage instance of newly created mail message */
-
+$url = \Yii::$app->urlManager->createUrl(['/api-empresa/validar-usuario','authKey' => $authKey]);
 ?>
 <h2>Olá, Seja Bem Vindo ao aplicativo E$TALECAS.</h2>
 <br>
 <h3>Clique no botão abaixo para validar sua conta:</h3>
-<?= Html::a('VALIDAR CONTA', ['api-empresa/validar-usuario', 'authKey' => $authKey], ['class' => 'btn btn-primary']) ?>
-<?= Html::button("<span class='glyphicon glyphicon-plus' aria-hidden='true'></span>",
-                    ['class'=>'kv-action-btn',
-                        'onclick'=>"window.location.href = '" . \Yii::$app->urlManager->createUrl(['/api-empresa/validar-usuario','authKey' => $authKey]) . "';",
-                        'data-toggle'=>'tooltip',
-                        'title'=>Yii::t('app', 'Create New Record'),
-                    ]
-                )?>
+
+<?= Html::a('VALIDAR CONTA', [$url, 'authKey' => $authKey], ['class' => 'btn btn-primary']) ?>
