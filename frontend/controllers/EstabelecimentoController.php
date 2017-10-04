7af892f276757850ffdc304399140bf9ae2e88f7
Charlan Santos <charlan.job@gmail.com>
2017-10-03 22:08:31 -0300

    iugu teste

--- frontend/controllers/EstabelecimentoController.php
+++ frontend/controllers/EstabelecimentoController.php
@@ -429,6 +429,9 @@ class EstabelecimentoController extends \common\controllers\GlobalBaseController
     // altera status da entrega
     public function setStatusDelivery($param) {
         $CB16PEDIDO = CB16PEDIDO::find()->where(['CB16_ID' => $param['pedido']])->andWhere('>','CB16_STATUS', 30)->all();
+        print '<pre>';
+        print_r($CB16PEDIDO);
+        die;
         if ($CB16PEDIDO) {
             $CB16PEDIDO->setAttribute('CB16_STATUS_DELIVERY', $param['new_status']);
             $CB16PEDIDO->save();     
